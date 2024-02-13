b320c965aaffe6086c729c3a0bf6fe64dd41a56e
Chris Montrois <chrismontrois@gmail.com>
2024-02-12 20:52:40 -0700

    Simplify next/prev hunk

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -81,27 +81,9 @@ return {
 						vim.keymap.set(mode, l, r, opts)
 					end
 
-					-- Jump to next hunk
-					map("n", "<leader>hn", function()
-						if vim.wo.diff then
-							return "]c"
-						end
-						vim.schedule(function()
-							gs.next_hunk()
-						end)
-						return "<Ignore>"
-					end, { expr = true })
-
-					-- Jump to prev hunk
-					map("n", "<leader>hp", function()
-						if vim.wo.diff then
-							return "[c"
-						end
-						vim.schedule(function()
-							gs.prev_hunk()
-						end)
-						return "<Ignore>"
-					end, { expr = true })
+					-- Navigation
+					map("n", "<leader>hn", gs.next_hunk)
+					map("n", "<leader>hp", gs.next_hunk)
 
 					-- Actions
 					map("n", "<leader>hu", gs.undo_stage_hunk)
